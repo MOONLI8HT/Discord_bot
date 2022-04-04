31fe0b5c4c2274cf7bf66c0ee04b5c1c99834bc2
MOONLI8HT <87700544+MOONLI8HT@users.noreply.github.com>
2022-04-04 13:20:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = 'Token' # bot token
+
+POST_ID = 0  # post id to read reactions from
+
+             #roles list according to emotes
+ROLES = {
+    '0' :0,  # role1
+    '1' :1'  # role 2
+             # etc.
+}
+
+             #exclude this roles from counting
+EXCROLES = ()
+
+MAX_ROLES_PER_USER = 2 # max ammount of roles a user can have
